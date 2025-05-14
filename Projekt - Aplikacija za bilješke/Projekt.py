e9fffbc6a818ca1a8c1b99767ad1d393ac29d2eb
Marko0D <dusic.m55@gmail.com>
2025-05-14 11:52:09 +0200

    Add files via upload

---
+++ Projekt - Aplikacija za bilješke/Projekt.py
@@ -0,0 +1,24 @@
+radi=True
+while radi==True:
+    print("---Jednostavna Bilježnica---")
+    print("1. Dodaj novu bilješku")
+    print("2. Pregledaj sve bilješke")
+    print("3. Izlaz")
+    print("Odaberite opciju (1-3)")
+    unos=int(input())
+    if unos==1:
+        a=input("Unesi tekst koji želiš dodati u bilježnicu:")
+        with open("biljeznica.txt", "a") as datoteka:
+                datoteka.write(a)
+                datoteka.write("\n")
+                datoteka.close()
+                print("Bilješka je uspješno spremljena.")
+    elif unos==2:
+        with open("biljeznica.txt", "r") as datoteka:
+                sadrzaj = datoteka.read()
+                print(sadrzaj)
+                datoteka.close()
+    elif unos==3:
+            radi=False
+            print("Doviđenja!")
+                
